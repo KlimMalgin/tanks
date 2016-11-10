329c7d16308a6af6a0f99c54778a2c9fe80686f0
Andrey <fxklim@gmail.com>
2016-11-10 14:28:40 +0000

    pixi.js empty project

---
+++ server.js
@@ -0,0 +1,20 @@
+
+var express = require('express');
+var app = new (express)()
+var host = process.env.IP;
+var port = process.env.PORT || 8080;
+
+app.use(express.static('libs'));
+app.use(express.static('app'));
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html')
+})
+
+app.listen(port, host, function(error) {
+  if (error) {
+    console.error(error)
+  } else {
+    console.info('==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.', port, port)
+  }
+})
