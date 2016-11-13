7e589be726fe99d306e0b10493def6e0f50b830c
Andrey <fxklim@gmail.com>
2016-11-13 06:13:02 +0000

    Поверхность для движения танков

--- app/index.js
+++ app/index.js
@@ -9,7 +9,7 @@ var Container = PIXI.Container,
 
 //Create the renderer
 var renderer = autoDetectRenderer(
-    512, 512,
+    588, 588,
     {antialias: false, transparent: false, resolution: 1}
 );
 
@@ -25,10 +25,31 @@ loader
 
 function setup() {
 
+    var tileW = 84,
+        tileH = 84,
+        tileCount = 7;
+
+    var textureTile = resources["public/tanks.sprite.json"].textures["tile.png"];
+
+    var tile = null;
+
+    for (var i = 0; i < tileCount; i++) {
+        for (var k = 0; k < tileCount; k++) {
+            tile = new Sprite(textureTile);
+            tile.position.set(tileW * i, tileH * k);
+            // TODO: Замостить поле тайлами
+            
+            stage.addChild(tile);
+        }
+    }
+
+
     var texture = resources["public/tanks.sprite.json"].textures["blue-tank.png"],
         sprite = new Sprite(texture);
 
     stage.addChild(sprite);
+    
+    
   
     renderer.render(stage);  
 }
