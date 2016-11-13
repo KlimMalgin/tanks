8b125cf96c3d44ce28d5db8d6706cc779a4fae2b
Andrey <fxklim@gmail.com>
2016-11-13 07:42:08 +0000

    Рефакторинг. Движение танка

--- app/index.js
+++ app/index.js
@@ -26,7 +26,35 @@ loader
 var spriteBlueTank;
 
 function setup() {
+    fillGameField();
+    initSprites();
+    gameLoop();
+}
 
+function initSprites() {
+    var textureBlueTank = resources["public/tanks.sprite.json"].textures["blue-tank.png"];
+        
+    spriteBlueTank = new Sprite(textureBlueTank);
+    
+    spriteBlueTank.vx = 0;
+    spriteBlueTank.vy = 0;
+}
+
+function gameLoop() {
+    requestAnimationFrame(gameLoop);
+    
+    spriteBlueTank.vx = 1;
+    spriteBlueTank.vy = 1;
+
+    spriteBlueTank.x += spriteBlueTank.vx;
+    spriteBlueTank.y += spriteBlueTank.vy;
+    
+    stage.addChild(spriteBlueTank);
+    
+    renderer.render(stage);
+}
+
+function fillGameField () {
     var tileW = 84,
         tileH = 84,
         tileCount = 7;
@@ -39,25 +67,7 @@ function setup() {
         for (var k = 0; k < tileCount; k++) {
             tile = new Sprite(textureTile);
             tile.position.set(tileW * i, tileH * k);
-            // TODO: Замостить поле тайлами
-            
             stage.addChild(tile);
         }
     }
-
-
-    var textureBlueTank = resources["public/tanks.sprite.json"].textures["blue-tank.png"];
-        
-    spriteBlueTank = new Sprite(textureBlueTank);
-
-    stage.addChild(spriteBlueTank);
-    
-    gameLoop();
-  
-}
-
-function gameLoop() {
-    requestAnimationFrame(gameLoop);
-    spriteBlueTank.x += 1;
-    renderer.render(stage);
 }
