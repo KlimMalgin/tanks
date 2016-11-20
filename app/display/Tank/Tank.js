bfbb55229516fb4e8bd0b00f97373f96b068c48d
Andrey <fxklim@gmail.com>
2016-11-20 15:08:12 +0000

    Класс танка

---
+++ app/display/Tank/Tank.js
@@ -0,0 +1,75 @@
+import { Sprite } from 'pixi.js';
+import Resources from '../../resources/Resources.js';
+
+
+export default class Tank extends Sprite {
+    constructor(name) {
+        super(Resources.getTexture(name));
+
+        /**
+         * Текущий угол поворота танка
+         */
+        this.rotation = 0;
+
+        /**
+         * Текущее направление
+         */
+        this.rotatePosition = 'up';
+
+        /**
+         * Ускорение при движении по горизонтали
+         */
+        this.vx = 0;
+
+        /**
+         * Ускорение при движении по вертикали
+         */
+        this.vy = 0;
+    }
+
+    /**
+     * Перемещает танк в указанном направлении с заданным ускорением
+     * @param {String} direction Направление перемещения {'up','down','left','right'}
+     * @param {Number} velocity Ускорение. По умолчанию == 2
+     */
+    go(direction, velocity = 2) {
+        var rotateAngle = Math.PI / 2;
+        switch (direction) {
+            case 'up':
+                this.rotation = rotateAngle * 0;
+                this.rotatePosition = direction;
+                this.vx = 0;
+                this.vy = velocity;
+                break;
+
+            case 'down':
+                this.rotation = rotateAngle * -2;
+                this.rotatePosition = direction;
+                this.vx = 0;
+                this.vy = velocity;
+                break;
+
+            case 'left':
+                this.rotation = rotateAngle * -1;
+                this.rotatePosition = direction;
+                this.vx = velocity;
+                this.vy = 0;
+                break;
+
+            case 'right':
+                this.rotation = rotateAngle * 1;
+                this.rotatePosition = direction;
+                this.vx = velocity;
+                this.vy = 0;
+                break;
+        }
+    }
+
+    /**
+     * Действия которые должны выполниться с объектов при перерисовке сцены
+     */
+    onDraw() {
+        this.x += this.vx;
+        this.y += this.vy;
+    }
+}
